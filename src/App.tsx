fbd232cc42a44d0ddb97aae5e6f9d76e3f1b2fb7
Dust07 <tienphat0707@gmail.com>
2022-04-11 17:26:39 +0700

    Init portfolio project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
 import './App.css';
-import LandingPage from './components/containers/LandingPage';
+import Footer from './components/containers/footer/Footer';
+import Header from './components/containers/header/Header';
+import Hero from './components/containers/hero/Hero';
+import LandingPage from './components/containers/landing-page/LandingPage';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Hero />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
