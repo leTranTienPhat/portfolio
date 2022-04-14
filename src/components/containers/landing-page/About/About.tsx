01d549888c19e345c8e4236bfa48de3aef86286f
Dust07 <tienphat0707@gmail.com>
2022-04-14 12:59:00 +0700

    test

--- src/components/containers/landing-page/About/About.tsx
+++ src/components/containers/landing-page/About/About.tsx
@@ -3,7 +3,20 @@ import "./style.scss"
 function About() {
   return (
     <div className="about-container">
-      <h2>About</h2>
+      <h2>Who am i?</h2>
+      <div className="about-desc">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque sed quisquam sapiente. At, reprehenderit. Mollitia provident impedit reprehenderit velit eligendi quae ducimus atque quo voluptas unde recusandae consequatur, necessitatibus odio.</p>
+      </div>
+
+      <div className="skills-gridview">
+        <div className="main-skills">
+
+        </div>
+        <div className="library">
+
+
+        </div>
+      </div>
     </div>
   )
 }
