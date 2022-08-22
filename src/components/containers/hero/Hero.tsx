0728ff539ecc61d3d8d3d8544da0646a15ad8244
Dust07 <tienphat0707@gmail.com>
2022-08-22 10:58:09 +0700

    Update resume

--- src/components/containers/hero/Hero.tsx
+++ src/components/containers/hero/Hero.tsx
@@ -6,13 +6,13 @@ import SocialList from "../../reusable-components/socialList/SocialList"
 
 const Hero = forwardRef((props, ref: any) => {
 
-  const myCVLink = "https://drive.google.com/file/d/1oC89jT1qogmt_DVK32VLM1z0T6peqrmR/view?usp=sharing"
+  const myCVLink = "https://drive.google.com/file/d/13L5SkxttKgW1WmXWWHX4LVCl-FiRe1Ik/view?usp=sharing"
   return (
     <div className="hero-container" ref={ref} id="section1">
       <div className="hero">
         <img className="hero-img hero-image-animation" src={require("../../../assets/images/hero-image.png")} alt="hero-img" />
         <div className="social-wrapper">
-          <button className="get-resume-btn" onClick={() => window.open(myCVLink)}>Get CV</button>
+          <button className="get-resume-btn" onClick={() => window.open(myCVLink)}>Get Resume</button>
           <SocialList />
         </div>
       </div>
